36cbf00e5f307f3e2173beae28593ba7e0c77b9f
Jan Burzinski <jan@burzinski.de>
2021-04-06 15:39:15 +0200

    added URL validation

--- server/src/handler/shortener.handler.ts
+++ server/src/handler/shortener.handler.ts
@@ -15,8 +15,25 @@ const generateId = (length: number) => {
   return result.join("");
 };
 
+const validURL = (str: string) => {
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
+  return !!pattern.test(str);
+};
+
 exports.create = async function (req: express.Request, res: express.Response) {
   const id = generateId(process.env.URL_ID_LENGTH);
+  if (!validURL(req.body.url)) {
+    res.status(400).send({ message: "URL not valid" });
+    return;
+  }
   await redis.redis.set(id, req.body.url);
   res.status(200).send({ message: "Successfully created", id: id });
 };
